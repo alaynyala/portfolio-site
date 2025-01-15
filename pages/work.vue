7608eab07ef48aa482bbe50f736fd76ee419d4f1
alaynyala <46248506+alaynyala@users.noreply.github.com>
2025-01-15 13:25:51 -0600

    updated pdf embed

--- pages/work.vue
+++ pages/work.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="work-page">
-    <iframe
-    :src="`https://mozilla.github.io/pdf.js/web/viewer.html?file=${encodeURIComponent('https://alainahunt.com/pdfs/alainahunt-selected2025.pdf')}`"
-    width="100%"
-    height="100vh"
-    style="border: none;"
-  ></iframe>
+    <VuePdfEmbed
+    :source="'https://alainahunt.com/pdfs/alainahunt-selected2025.pdf'"
+    :width="100%"
+    :height="100vh"
+    style="max-width: 100%;"
+  />
   </div>
 </template>
 
 <script>
+import VuePdfEmbed from 'vue-pdf-embed';
 export default {
+  components: {
+    VuePdfEmbed
+  },
   name: 'WorkPage'
 }
 </script>
